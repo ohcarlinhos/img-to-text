425963b7987a3539a3a8376833cf286f73b9f469
Carlos Roberto <crpu4a@gmail.com>
2023-10-21 17:53:20 -0300

    feat: add async errors suport and erro middleaware to setup

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,28 @@
-import express from "express";
+import "express-async-errors";
+import express, { NextFunction, Request, Response } from "express";
 import path from "path";
 
 import { imageToTextController } from "./imageToText";
 import { uploadMiddleware } from "./middlewares";
+import { AppError } from "./error";
 
 const app = express();
 app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, "..", "public")));
 app.post("/img", uploadMiddleware.single("file"), imageToTextController);
+
+app.use((error: Error, _: Request, res: Response, __: NextFunction) => {
+  let message = "Internal server error";
+  let statusCode = 500;
+
+  if (error instanceof AppError) {
+    message = error.message;
+    statusCode = error.statusCode;
+  }
+
+  return res.status(statusCode).json({ message });
+});
 
 const PORT = 3000;
 
