2b106267320c562eeb5d0b65c8bcb7e1a39ad364
Carlos Roberto <crpu4a@gmail.com>
2023-10-21 17:54:19 -0300

    feat: adapt interface to handle with API errors

--- public/global.js
+++ public/global.js
@@ -2,29 +2,41 @@ const form = document.querySelector("form");
 const submitButton = document.querySelector("#submit-button");
 const fileInput = document.querySelector("#file-input");
 
-const addTextToView = (text) => {
+const addTextToView = (text, error = false) => {
   const textElement = document.querySelector("#text");
+  let className = "p-4 mb-3 border-b border-gray-250";
+
+  if (error) className += " bg-red-100";
+
   textElement.textContent = text;
-  textElement.className = "p-4 mb-3 border-b border-gray-250";
+  textElement.className = className;
 };
 
 addTextToView("This is a example text!");
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
   const formData = new FormData();
   formData.append("file", fileInput.files[0]);
 
-  submitButton.disabled = true;
-  fetch("/img", {
-    method: "POST",
-    body: formData,
-  })
-    .then((res) => res.json())
-    .then(({ text }) => {
-      addTextToView(text);
-    })
-    .finally(() => {
-      submitButton.disabled = false;
+  try {
+    submitButton.disabled = true;
+
+    const res = await fetch("/img", {
+      method: "POST",
+      body: formData,
     });
+
+    if (!res.ok) {
+      const { message } = await res.json();
+      throw new Error(message);
+    }
+
+    const { text } = await res.json();
+    addTextToView(text);
+  } catch (err) {
+    addTextToView(err.message || "Error! Please try again.", true);
+  } finally {
+    submitButton.disabled = false;
+  }
 });
