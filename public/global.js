a35cfe786d76e6198b25f791990f89acbd5f9916
Carlos Roberto <crpu4a@gmail.com>
2023-10-26 19:10:47 -0300

    feat: add copy text button

--- public/global.js
+++ public/global.js
@@ -2,18 +2,28 @@ const form = document.querySelector("form");
 const submitButton = document.querySelector("#submit-button");
 const fileInput = document.querySelector("#file-input");
 const selectedImage = document.querySelector("#selected-image");
+const copyButton = document.querySelector("#copy-button");
 
 const showImage = (e) => {
   const { files } = e.clipboardData;
   if (!files || !files.length) return;
 
   fileInput.files = e.clipboardData.files;
+  if (selectedImage.classList.length) selectedImage.classList.remove("hidden");
+
   selectedImage.src = URL.createObjectURL(e.clipboardData.files[0]);
+};
+
+const copyText = async () => {
+  const textElement = document.querySelector("#text");
+  const text = textElement.textContent;
+  await navigator.clipboard.writeText(text.replace(/(\r\n|\n|\r)/gm, " "));
 };
 
 window.addEventListener("paste", showImage);
 
 fileInput.addEventListener("change", showImage);
+copyButton.addEventListener("click", copyText);
 
 const addTextToView = (text, error = false) => {
   const textElement = document.querySelector("#text");
